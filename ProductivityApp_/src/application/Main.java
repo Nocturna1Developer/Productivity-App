c6050de8126f14cf7d0149c8c5d117a94ca389de
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2021-10-30 15:26:08 -0700

    Updated Timer

--- ProductivityApp_/src/application/Main.java
+++ ProductivityApp_/src/application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("/ProductivityApp_/src/application/LoginPage.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
